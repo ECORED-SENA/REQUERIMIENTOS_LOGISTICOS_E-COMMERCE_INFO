d202a7a1762f9b3be834edda621989d104c0eaed
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-05-31 07:34:26 -0500

    Cargue inicial de archivos

---
+++ src/views/Inicio.vue
@@ -0,0 +1,148 @@
+<template lang="pug">
+section
+  BannerPrincipal.mb-5
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 1. Presentación
+
+    p El programa de formación complementaria virtual Apropiación de requerimientos logísticos en el e-commerce, está dirigido a directivos generales (gerentes, directores y jefes), personal de operación y de cargos tácticos, además de los operativos de empresas prestadoras de servicios logísticos, interesados en interpretar requerimientos y normativa en la logística de distribución para el e-commerce, de tal forma que puedan contar con los elementos necesarios para la planeación en estas nuevas modalidades de negociación dentro de una cobertura local y nacional, diferenciando las exigencias del mercado para incorporarse a negociaciones de alcance internacional. Igualmente, podrá conocer componentes de soporte para la prestación del servicio logístico, así como para la utilización de las denominadas pasarelas de pago, reconociendo los compromisos inherentes a este tipo de servicios.
+
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 2. Justificación<br>del programa
+
+    p La Organización de los Estados Americanos (OEA) desarrolla una iniciativa para América Latina, en la que se busca apoyar a los empresarios en el uso estratégico de las Tecnologías de la Información y Comunicación (TIC) para contribuir al cierre de la brecha digital de especial relevancia en el contexto actual en el que la pandemia del Covid-19 ha impactado los hábitos de consumo en línea, forzando a empresarios y negocios a buscar formas innovadoras de ofrecer sus bienes y servicios. El Ministerio de Tecnologías de la Información y las Comunicaciones de Colombia (MinTIC) y la OEA presentaron un plan para ayudar a que las micro, pequeñas y medianas empresas (MIPYME) colombianas puedan, sin costo alguno, empezar a vender sus productos por internet (MinTIC, 2020). 
+      br
+      br
+      | En este sentido, atendiendo a la necesidad de capacitación en los temas asociados al e-commerce, se encuentra cómo la Oficina Internacional del Trabajo (OIT), frente a la globalización económica, identifica la necesidad de incorporar temáticas asociadas al comercio electrónico en las capacitaciones, ya que se demanda en el mercado trabajadores con niveles altos y específicos de formación para atender la segmentación de los consumidores y las altas exigencias en la calidad de los servicios prestados; para el caso específico del área logística, se requiere tener en cuenta la capacitación en estos aspectos (OIT, 1999). 
+      br
+      br
+      | Por lo anterior, y tomando como referente el estudio realizado por el Observatorio de Comercio Electrónico en Colombia, se observó que el 98 % de la población estudiada en nueve (9) ciudades usa internet y que el 91 % de las personas que tienen este acceso realizan actividades de compra; el 19 % realiza actividades completas de e-commerce (compra, facturación y pago) y el 90 % de los compradores finales optan por logística de entrega domiciliaria; por otra parte, se identificó un número aproximado de 35.000 tiendas online que corresponden a pequeños y medianos negocios (Observatorio de Comercio Electrónico en Colombia, 2019). 
+      br
+      br
+      | Además, se puede encontrar que la Organización Mundial del Comercio (OMC) aporta la definición del término de comercio electrónico como “... la producción, distribución, comercialización, venta o entrega de bienes y servicios por medios electrónicos”. En mayo de 1998, este organismo, reconociendo que la expansión del comercio electrónico mundial estaba creando nuevas oportunidades para el comercio, emitió la declaración sobre el comercio electrónico mundial. En ella se pedía el establecimiento de un programa de trabajo sobre el comercio electrónico, que fue adoptado en septiembre de 1998 (OMC, 1998). 
+      br
+      br
+      | Se hace necesario, entonces, indicar que los orígenes del e-commerce se remontan al año 1969, en primera instancia el concepto de comercio electrónico surge en la década de los 70 gracias a la aparición de la transferencia electrónica de fondos (EFT - Electronic Fund Transfer), posteriormente y gracias a la transferencia electrónica de datos (EDI - Electronic Data Interchange) se permite el intercambio recíproco de documentos de carácter comercial, administrativo y contable en formatos normalizados. Desde el año 2000, se tiene relación del concepto de e-commerce en los diversos contextos de comercio electrónico (B2B, B2C, B2G, entre otros), pero no es sino hasta la creación de la tecnología web y su uso empresarial que se genera la oportunidad de masificación del comercio electrónico; así es cómo puede evidenciarse que se apropia el concepto de e-commerce, cuando el negocio en línea (compra-venta), además de ser efectuado gracias a los medios electrónicos, permite la incorporación del pago y su correspondiente facturación, al igual que se posibilita la concertación de formas, tiempos y medios de entrega (Fernández, 2015). 
+      br
+      br
+      | En este sentido, el Servicio Nacional de Aprendizaje (SENA) presenta el programa de formación complementaria en Apropiación de requerimientos logísticos en el e-commerce, con el objetivo de brindar las herramientas cognitivas apropiadas a cada aprendiz de manera que pueda desarrollar competencias asociadas a la logística, frente a las expectativas de los sectores productivos en el territorio nacional. 
+      br
+      br
+      | Esta es una oferta abierta, dirigida a directivos, personal de operación, cargos tácticos y operativos de empresas prestadoras de servicios logísticos, interesados en interpretar requerimientos y normativa en la logística de distribución para el e-commerce.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 3. Competencias a <br>desarrollar
+
+    p 210101066 - Diseñar plan de distribución según canal y normativa de transporte.
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 4. Perfil de<br> ingreso
+
+    p.mb-4 Se requiere que el aprendiz tenga dominio básico en el manejo de herramientas informáticas y de comunicación como: correo electrónico, chats, procesadores de texto, software para presentaciones, navegadores de internet, otros sistemas y herramientas tecnológicas necesarias para la formación en ambientes virtuales.    
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Perfil de<br>egreso
+
+    p.mb-4 El egresado(a) de formación complementaria virtual Apropiación de requerimientos logísticos en el e-commerce estará en capacidad de manejar sistemas de información en comercio electrónico que permitan innovar y mejorar la optimización de los recursos generando mayores oportunidades de ingresos para la empresa.     
+
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.pb-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 5. Estrategia<br>metodológica
+
+    p.mb-4 Centrada en la construcción de autonomía para garantizar la calidad de la formación en el marco de la formación por competencias, el aprendizaje por proyectos y el uso de técnicas didácticas activas que estimulan el pensamiento para la resolución de problemas simulados y reales; soportadas en el utilización de las tecnologías de la información y la comunicación, integradas, en ambientes virtuales de aprendizaje, que en todo caso recrean el contexto productivo y vinculan al aprendiz con la realidad cotidiana y el desarrollo de las competencias. 
+      br
+      br
+      | Igualmente, debe estimular de manera permanente la autocrítica y la reflexión del aprendiz sobre el que hacer y los resultados de aprendizaje que logra a través de la vinculación activa de las cuatro fuentes de información para la construcción de conocimiento:   
+
+    ul.fa-ul
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El instructor - tutor
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El entorno
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] Las TIC
+      li.mb-2 #[span.li--clr.fa-li #[i.fas.fa-angle-right ]] El trabajo colaborativo   
+
+
+
+  //- Créditos
+  .container.tarjeta.tarjeta__template--azul-claro.px-3.py-4.px-sm-5.mb-5
+    .titulo__template--a.mb-4
+      span.h6 Créditos
+
+    CreditosComp
+
+  Footer
+  
+
+</template>
+<script>
+import { menuPrincipal } from '../config/global'
+import BannerPrincipal from '../components/plantilla/BannerPrincipal'
+import CreditosComp from '../components/plantilla/CreditosComp'
+import Footer from '../components/plantilla/Footer'
+export default {
+  name: 'Inicio',
+  components: {
+    BannerPrincipal,
+    CreditosComp,
+    Footer,
+  },
+  data: () => ({
+    menuPrincipalData: menuPrincipal,
+  }),
+  computed: {
+    desarrolloContenidosData() {
+      const allMenuData = [
+        ...this.menuPrincipalData.menu,
+        ...this.menuPrincipalData.subMenu,
+      ]
+      return allMenuData.filter(item => item.desarrolloContenidos)
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+.resultados-aprendizaje
+  &__item
+    margin-bottom: 20px
+    &:last-child
+      margin-bottom: 0
+
+.desarrollo-contenidos
+  &__item
+    display: flex
+    align-items: center
+    justify-content: space-between
+    margin-bottom: 25px
+    padding: 5px
+    border-radius: $base-border-radius
+
+    &__texto
+      display: flex
+      align-items: center
+
+    &__icono
+      display: block
+      background-color: $color-sistema-g
+      font-size: 20px
+      font-weight: $base-black-font-weight
+      text-align: center
+      padding: 10px
+      line-height: 1em
+      width: 40px
+      margin-right: 15px
+
+    &:last-child
+      margin-bottom: 0
+
+    &:hover
+      background-color: $color-sistema-g
+      font-weight: $base-black-font-weight
+      color: $color-sistema-a
+</style>
